a68fb45f5375cb6ee081781e7cb2163db6918846
kennethEdwin147 <edwingbeti@gmail.com>
2022-06-16 22:29:45 -0400

    Commencement de la personnalisation stylistique de la fenêtre modale

--- script/message.js
+++ script/message.js
@@ -1,13 +1,38 @@
 (function() {
-    let elmBoite__modale = document.createElement('div'),
-        elmBody = document.querySelector('body'),
-        elmOk = document.querySelector('#mon_bouton');
+    let elmBoite__modale,
+        elmBoite__x
 
-    elmBoite__modale.classList.add('boite__modale');
-    elmBody.appendChild(elmBoite__modale);
+    function creer_boite_modale() {
+        
+        elmBoite__modale = document.createElement('div');
+        elmBoite__x = document.createElement('button');
+        elmBody = document.querySelector('body');
+
+        elmBoite__modale.appendChild(elmBoite__x)
+
+        elmBoite__modale.classList.add('boite__modale');
+        elmBoite__x.classList.add('boite__x');
+        elmBoite__x.innerHTML = "X";
+
+        elmBody.appendChild(elmBoite__modale);
+    }
+ 
+    creer_boite_modale();
+    let elmOk = document.querySelector('#mon_bouton');
+
 
     elmOk.addEventListener('mousedown', function() {
-        console.log('Brave fonctionne')
+        elmBoite__modale.classList.add('boite__modale__ouvrir');
     });
+
+    
+    elmBoite__x.addEventListener('mousedown', function() {
+        elmBoite__modale.classList.remove('boite__modale__ouvrir');
+    });
+
+
+
+
+
     
 })();
